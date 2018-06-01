9aba4f8dce20dae089d5d61026cf35222aca48b6
Tree <lincf97@163.com>
2018-06-01 19:26:35 +0800

    新增文件

--- Django_api/urls.py
+++ Django_api/urls.py
@@ -20,4 +20,6 @@ from sign import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', views.index),
+    path('login_action/', views.login_action),
+    path('event_manage/', views.event_manage),
 ]
--- sign/views.py
+++ sign/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
-from django.http import  HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request, "index.html")
+
+# 登录动作
+
+
+def login_action(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        if username == 'admin' and password == '123':
+            response = HttpResponseRedirect('/event_manage/')
+            request.session['user'] = username # 将session
+            return response
+        else:
+            return render(request, 'index.html', {'error':'username or password error!'})
+
+# 发布会管理
+
+
+def event_manage(request):
+    username = request.session.get('user', '')
+    return render(request, "event_manage.html", {'user': username})
